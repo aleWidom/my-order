a56065691214b301e3e8699a4a6412800df7b0f1
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-03-04 20:37:32 -0300

    prueba-cambio

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,7 +7,7 @@ import styles from './Welcome.module.css'
 const Welcome = ({ tableNumber, setTableNumber }) => {
   return (
     <div className={styles.welcome}>
-      <h3 className={styles.header}>Bienvenidos...</h3>
+      <h3 className={styles.header}>Bienvenidos....</h3>
       <Brand clases={styles.brand} />
       <Input tableNumber={tableNumber} setTableNumber={setTableNumber} />
     </div>
