885e18612fbc94fd683af6c2f501aa70b4cc3199
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-05-25 18:00:33 -0300

    changes-tables-lambda

--- amplify/backend/function/functionLambdaTables/src/index.js
+++ amplify/backend/function/functionLambdaTables/src/index.js
@@ -18,22 +18,25 @@ exports.handler = async (event) => {
 
     let result;
 
-    if (event.queryStringParameters?.active) {
+
+    const promiseQuery = new Promise((resolve) => {
+        connection.query(`SELECT * from Table_`, function (error, results, fields) {
+            resolve(results)
+        });
+    })
+    result = await promiseQuery
+
+
+    if (event.queryStringParameters?.active !== undefined) {
         const promiseQuery = new Promise((resolve) => {
             connection.query(`select * from Table_ Where table_active = 1 && id_restaurant = 1;`, function (error, results, fields) {
                 resolve(results)
             });
         })
         result = await promiseQuery
-    } else if (event.pathParameters) {
-        if (event.pathParameters.proxy === `/call/{id}`) {
-            const promiseQuery = new Promise((resolve) => {
-                connection.query(`UPDATE Table_ Set table_call = 1 Where id = 2  `, function (error, results, fields) {
-                    resolve(results)
-                });
-            })
-            result = await promiseQuery
-        } else {
+    }
+    else if (event.queryStringParameters?.activate !== undefined) {
+        if (event.queryStringParameters.activate === "activate") {
             const promiseQuery = new Promise((resolve) => {
                 connection.query(`UPDATE Table_ Set table_active = 1 Where id = ${event.pathParameters.proxy}  `, function (error, results, fields) {
                     resolve(results)
@@ -41,14 +44,27 @@ exports.handler = async (event) => {
             })
             result = await promiseQuery
         }
-    } else {
-        const promiseQuery = new Promise((resolve) => {
-            connection.query(`SELECT * from Table_`, function (error, results, fields) {
-                resolve(results)
-            });
-        })
-        result = await promiseQuery
     }
+    else if (event.queryStringParameters?.call !== undefined) {
+        if (event.queryStringParameters?.call === "call") {
+            const promiseQuery = new Promise((resolve) => {
+                connection.query(`UPDATE Table_ Set table_call= 1 Where id = ${event.pathParameters.proxy}  `, function (error, results, fields) {
+                    resolve(results)
+                });
+            })
+            result = await promiseQuery
+        }
+        else if (event.queryStringParameters?.call === "notCall") {
+            const promiseQuery = new Promise((resolve) => {
+                connection.query(`UPDATE Table_ Set table_call= 0 Where id = ${event.pathParameters.proxy}  `, function (error, results, fields) {
+                    resolve(results)
+                });
+            })
+            result = await promiseQuery
+        }
+    }
+
+
 
     return {
         statusCode: 200,
