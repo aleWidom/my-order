3cf9e25fe2496535e34fcf9c570343b67484e255
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-09-23 21:14:57 -0300

    amplify-lambda-update-item

--- amplify/backend/function/functionLambdaRestaurants/src/index.js
+++ amplify/backend/function/functionLambdaRestaurants/src/index.js
@@ -1,7 +1,7 @@
 /**
-  *
-  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
-  */
+ *
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
 
 
 exports.handler = async (event) => {
@@ -9,22 +9,51 @@ exports.handler = async (event) => {
     var mysql = require('mysql');
 
     var connection = mysql.createConnection({
-        host:
-            'myorderdatabase.cluster-ctulrcqrkejd.us-east-1.rds.amazonaws.com',
+        host: 'myorderdatabase.cluster-ctulrcqrkejd.us-east-1.rds.amazonaws.com',
         user: 'admin',
         password: 'admin123456',
         database: 'myorder'
     });
 
+    let result;
+
     const promiseQuery = new Promise((resolve) => {
-        connection.query('SELECT * from Restaurant', function
-            (error, results, fields) {
+        connection.query(`SELECT * from Item Where id_restaurant = 1 ;`, function (error, results, fields) {
             resolve(results)
         });
     })
+    result = await promiseQuery
 
-    const result = await promiseQuery
 
+    if (event.queryStringParameters?.categories !== undefined) {
+        const promiseQuery = new Promise((resolve) => {
+            connection.query(`SELECT * from Category`, function (error, results, fields) {
+                resolve(results)
+            });
+        })
+        result = await promiseQuery
+    } else if (event.queryStringParameters?.search !== undefined) {
+        const promiseQuery = new Promise((resolve) => {
+            connection.query(`select * from Item where title like "%${event.queryStringParameters.search}%"`, function (error, results, fields) {
+                resolve(results)
+            });
+        })
+        result = await promiseQuery
+    } else if (event.queryStringParameters?.ranking !== undefined) {
+        const promiseQuery = new Promise((resolve) => {
+            connection.query(`SELECT * FROM Item WHERE id IN(1,4,8,7)`, function (error, results, fields) {
+                resolve(results)
+            });
+        })
+        result = await promiseQuery
+    } else if (event.queryStringParameters?.dayPlates !== undefined) {
+        const promiseQuery = new Promise((resolve) => {
+            connection.query(`SELECT * FROM Item WHERE id IN(2,5,6,3)`, function (error, results, fields) {
+                resolve(results)
+            });
+        })
+        result = await promiseQuery
+    }
 
     return {
         statusCode: 200,
