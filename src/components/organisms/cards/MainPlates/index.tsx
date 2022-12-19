a383a0769a2654be36125e666b1fada6e18696d6
alejandro <widomlanski@hotmail.com>
2022-12-19 10:49:06 -0300

    positio sticky mis solicitud

--- src/components/organisms/cards/MainPlates/index.tsx
+++ src/components/organisms/cards/MainPlates/index.tsx
@@ -3,6 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { MainPlate } from "../../../molecules";
 import { Plate } from "../../../../interfaces";
 import styles from "./MainPlates.module.scss";
+import "swiper/css/navigation";
+import "swiper/css";
 
 interface Props {
   cardsDayPlates: Plate[];
@@ -21,9 +23,29 @@ export const MainPlates: FC<Props> = ({
         <h3 className={styles.title}>Platos del día</h3>
         <Swiper
           slidesPerView={1.2}
-          spaceBetween={20}
+/*           spaceBetween={20} */
         >
           {cardsDayPlates.map((e) => (
+            <SwiperSlide key={e.id}>
+              <MainPlate
+                key={e.id}
+                source={e.photo}
+                header={e.title}
+                price={e.price}
+                description={e.description}
+                id={e.id}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div  className={styles.cardsPlates}>
+        <h3 className={styles.title}>Platos más solicitados</h3>
+        <Swiper
+          slidesPerView={1.2}
+          spaceBetween={20}
+        >
+          {cardsRankingPlates.map((e) => (
             <SwiperSlide key={e.id}>
               <MainPlate
                 key={e.id}
